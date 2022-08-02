bcbb3da813e5febe9efb6d35229c85a3eea8db7c
LucasKatz <105380322+LucasKatz@users.noreply.github.com>
2022-08-01 21:22:29 -0300

    Delete tp1.js

--- tp1.js
+++
@@ -1,7 +0,0 @@
-let ingresarNumero = parseInt(prompt("Ingresar Numero del 1 al 10"));
-
-for (let i = 1; i <= ingresarNumero; i++) {
-    let resultado = ingresarNumero + i ;
-    console.log("Hola Mundo" + " " + i);
-}
-
