1da928c8afeb6490062070e1e8dc35c56862d998
LucasKatz <105380322+LucasKatz@users.noreply.github.com>
2022-08-06 20:30:58 -0300

    Add files via upload

--- productos.js
+++ productos.js
@@ -12,91 +12,91 @@ const menu = [
     {
         nombre: "brownies con dulce de leche",
         precio: "$ 180",
-        //fuente:<img src="../img/Budines/Brownie.jpg" class="card-img-top" alt="..."></img>,
+        fuente: src="../img/Budines/Brownie.jpg"
     },
 
     {
         nombre:"brownies con nuez",
         precio:"$200",
-        //fuente:<img src="../img/Budines/brownies.jpg" class="card-img-top" alt="..."></img>,
+        fuente: src= "../img/Budines/brownies.jpg"
     },
-
     {
         nombre:"magdalenas",
         precio: "$400",
-        //fuente:<img src="../img/Budines/magdalenas.jpg" class="card-img-top" alt="..."></img>,
+        fuente: src= "../img/Budines/magdalenas.jpg"
     },
 
     {
         nombre: "cookies de chocolate",
         precio: "$220",
-        //fuente:<img src="../img/Cookies/coockies choco.jpg" class="card-img-top" alt="..."></img>,
+        fuente: src= "../img/Cookies/cookieschoco.jpg"
     },
 
     {
         nombre: "cookies con chips",
         precio: "$ 250",
-        //fuente:<img src="../img/Cookies/cookies con chips.jpg" class="card-img-top" alt="..."></img>,
+        fuente: src="../img/Cookies/cookiesconchips.jpg"
     },
 
     {
         nombre: "cookies surtidas",
         precio: "$200",
-        //fuente:<img src="../img/Cookies/cookies varias.jpg" class="card-img-top" alt="..."></img>,
+        fuente: src="../img/Cookies/cookiesvarias.jpg"
     },
+
 
     {
         nombre: "pan de salvado",
         precio: "$350",
-        //fuente:<img src="../img/Panaderia/pan de salvado.jpg" class="card-img-top" alt="..."></img>,
+        fuente: src= "../img/Panaderia/pandesalvado.jpg"
     },
 
     {
         nombre:"trenza de pan sin semillas",
         precio: "$350",
-        //fuente:<img src="../img/Panaderia/pan trenza 2.jpg" class="card-img-top" alt="..."></img>,
+        fuente: src="../img/Panaderia/pantrenza2.jpg"
     },
 
     {
         nombre:"trenza de pan con semillas",
         precio: "$400",
-        //fuente:<img src="../img/Panaderia/pan semillas.jpg" class="card-img-top" alt="..."></img>,
+        fuente: src="../img/Panaderia/pansemillas.jpg"
     },
 
     {
         nombre:"cañoncitos",
         precio: "$550",
-        //fuente:<img src="../img/Facturas/cañoncitos.jpg" class="card-img-top" alt="..."></img>,
+        fuente: src="../img/Facturas/cañoncitos.jpg"
     },
 
     {
         nombre:"vigilantes",
         precio: "$500",
-        //fuente:<img src="../img/Facturas/facturas varias 2.jpg" class="card-img-top" alt="..."></img>,
+        fuente: src="../img/Facturas/facturasvarias2.jpg"
     },
 
     {
         nombre:"medialunas",
         precio: "$480",
-        //fuente:<img src="../img/Facturas/medialunas.jpg" class="card-img-top" alt="..."></img>,
+        fuente: src="../img/Facturas/medialunas.jpg"
     },
 
     {
         nombre:"calamares rellenos",
         precio: "$600",
-        //fuente:<img src="../img/Comida Salada/calamares rellenos.jpg" class="card-img-top" alt="..."></img>,
+        fuente: src="../img/ComidaSalada/calamaresrellenos.jpg"
     },
 
     {
         nombre:"locro",
         precio: "$900",
-        //fuente:<img src="../img/Comida Salada/locro3.jpg" class="card-img-top" alt="..."></img>,
+        fuente: src="../img/ComidaSalada/locro3.jpg"
     },
 
     {
         nombre:"matambre",
         precio: "$650",
-        //fuente:<img src="../img/Comida Salada/matambre casero.jpg" class="card-img-top" alt="..."></img>,
+        fuente: src="../img/ComidaSalada/matambrecasero.jpg"
     }
     ];
 
@@ -104,10 +104,10 @@ const menu = [
 let cards=document.getElementById("galeria");
 for(const imagen of menu){
     let card=document.createElement("div");
-    card.className="card col-md-3";
+    card.className="card col-3 text-align-center";
     card.innerHTML=`
-        <div class="card-body">
-            <img src= ${imagen.fuente} class="card-img-top" alt="...">
+    <div class="card" style="width: 18rem;">
+    <img src= ${imagen.fuente} class="card-img-top" alt="...">
             <h5 class="card-title">${imagen.nombre}</h5>
             <p class="card-text">${imagen.precio}</p>
             <a href="#" class="btn btn-primary">COMPRAR</a>
